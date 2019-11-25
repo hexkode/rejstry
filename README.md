4dac91e1643a172eece1f8cea7b371178f091649
hexkode <hexkode@gmail.com>
2019-11-25 15:57:41 -0800

    Update readme

--- README.md
+++ README.md
@@ -1,26 +1,54 @@
-# rejstry
-Rejstry Framework
+# Rejstry Framework
+Opinionated Nodejs framework with the assumption that every component is a package.
 
 ## Example
 ```javascript
 const { register, registry } = require('rejstry');
+const pino = require('pino');
+const express = require('express');
 
 const packages = {
+  // config and logger are kernel packages that are required.
   config: () => ({
-    NODE_ENV: 'development',
+    NODE_ENV: process.env.NODE_ENV,
+    SERVER_A_PORT: 3000,
+    SERVER_B_PORT: 4000,
+    SERVER_C_PORT: 5000,
   }),
-  logger: () => ({
-    trace: msg => console.log(msg),
-    debug: msg => console.log(msg),
-    info: msg => console.log(msg),
-    warn: msg => console.log(msg),
-    error: msg => console.error(msg),
-  }),
+  logger: () => pino(),
+  // example packages
+  serverA: () => {
+    const app = express();
+    app.get('/', (req, res) => res.send(`serverA@${registry.packages.config.SERVER_A_PORT}`));
+    app.set('title', 'serverA');
+    app.listen(registry.packages.config.SERVER_A_PORT);
+    return app;
+  },
+  serverB: () => {
+    const app = express();
+    app.get('/', (req, res) => res.send(`serverB@${registry.packages.config.SERVER_B_PORT}`));
+    app.set('title', 'serverB');
+    app.listen(registry.packages.config.SERVER_B_PORT);
+    return app;
+  },
+  serverC: () => {
+    const app = express();
+    app.get('/', (req, res) => res.send(`serverC@${registry.packages.config.SERVER_C_PORT}`));
+    app.set('title', 'serverC');
+    app.listen(registry.packages.config.SERVER_C_PORT);
+    return app;
+  },
 };
 
 (async () => {
   await register({
     packages,
+    // filter a list of packagesIds to register in order
+    packagesFilter: packagesIds => [
+      'serverC',
+      // 'serverB',
+      'serverA',
+    ],
   }).catch((err) => {
     const errMessage = 'Encountered an exception.  Exit with process.exit(1).';
 
@@ -35,9 +63,11 @@ const packages = {
     process.exit(1);
   });
 
-  // Can access all the registered packages after register()
-  // Even in another file, just import:
-  // const { registry, package } = require('rejstry');
-  registry.packages.logger.info(registry.packages.config.NODE_ENV);
+  // After register(), all the registered packages are
+  // accessible by including the following in any file:
+  // const { registry, packages } = require('rejstry');
+  registry.packages.logger.info(registry.packages.config);
+  registry.packages.logger.info(registry.packages.serverA.get('title'));
+  registry.packages.logger.info(registry.packages.serverC.get('title'));
 })();
 ```
--- src/index.js
+++ src/index.js
@@ -1,66 +1,64 @@
 /* eslint-disable no-restricted-syntax, no-await-in-loop */
-const fs = require('fs');
-
 const KERNEL_PACKAGES = ['config', 'logger'];
 
 const registry = {
   packages: {},
 };
 
+const validateKernelPackages = (packages) => {
+  for (const pkgId of KERNEL_PACKAGES) {
+    if (packages[pkgId]) {
+      if (typeof packages[pkgId] !== 'function') {
+        throw new Error(`Kernel package.${pkgId} is not a function.`);
+      }
+    } else {
+      throw new Error(`Kernel package.package.${pkgId} is required.`);
+    }
+  }
+};
+
 const register = async ({
-  packages,
-  packagesFilter,
+  packages = {},
+  packagesFilter = packageIds => packageIds.filter(packageId => !KERNEL_PACKAGES.includes(packageId)),
 }) => {
-  if (typeof packages.config !== 'function') throw new Error('Kernel package config is required.');
-  if (typeof packages.logger !== 'function') throw new Error('Kernel package logger is required.');
+  validateKernelPackages(packages);
 
   registry.packages.config = packages.config();
   registry.packages.logger = packages.logger();
-  const { logger } = registry.packages;
 
+  const { logger } = registry.packages;
   logger.info('Preparing registry...');
-  logger.info('config is registered.');
-  logger.info('logger is registered.');
+  logger.info('package.config is registered.');
+  logger.info('package.logger is registered.');
 
   const packageIds = Object.keys(packages) || [];
-  const pkgIds = packagesFilter
-    ? packagesFilter(packageIds)
-    : packageIds.filter(packageId => !KERNEL_PACKAGES.includes(packageId));
-  for (const pkgId of pkgIds) {
-    if (pkgId) {
-      logger.info(`Registering packages.${pkgId}.`);
+  const regPkgIds = packagesFilter(packageIds);
+  for (const regPkgId of regPkgIds) {
+    if (regPkgId) {
+      logger.info(`Registering packages.${regPkgId}.`);
 
-      const pkgRegister = (packages && packages[pkgId]) || undefined;
+      const pkgRegister = packages[regPkgId];
       if (pkgRegister) {
         if (typeof pkgRegister === 'function') {
-          registry.packages[pkgId] = await pkgRegister();
-          registry.packages[pkgId].id = pkgId;
-          logger.info(`packages.${pkgId} is registered.`);
+          registry.packages[regPkgId] = await pkgRegister();
+          logger.info(`packages.${regPkgId} is registered.`);
         } else {
-          logger.warn(`Failed to register.  packages.${pkgId} is not a function.`);
+          logger.warn(`Failed to register.  packages.${regPkgId} is not a function.`);
         }
       } else {
-        logger.warn(`Failed to register.  packages.${pkgId} not found.`);
+        logger.warn(`Failed to register.  packages.${regPkgId} not found.`);
       }
     }
   }
 
-  logger.info(Object.keys(registry.packages), 'Packages registered.  Registry is ready.');
+  logger.info(Object.keys(registry.packages), 'Registered packages.');
+  logger.info('Registry is ready.');
+
   return registry;
 };
 
-/* eslint-disable global-require, import/no-dynamic-require, no-shadow */
-const loader = dirname => fs.readdirSync(dirname)
-  .filter(dir => dir !== dirname)
-  .reduce((packages, packageDir) => ({
-    ...packages,
-    // packageId is set to package dir
-    [packageDir]: require(`./${packageDir}`),
-  }), {});
-
 module.exports = {
-  register,
   registry,
-  loader,
   packages: registry.packages,
+  register,
 };
