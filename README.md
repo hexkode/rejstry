777d71864d6c8a3dc3d72c8e784a3a0ed02cf349
hexkode <hexkode@gmail.com>
2019-11-24 17:16:33 -0800

    First alpha version

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # rejstry
 Rejstry Framework
+
+## Example
+```javascript
+const { register, registry } = require('rejstry');
+
+const packages = {
+  config: () => ({
+    NODE_ENV: 'development',
+  }),
+  logger: () => ({
+    trace: msg => console.log(msg),
+    debug: msg => console.log(msg),
+    info: msg => console.log(msg),
+    warn: msg => console.log(msg),
+    error: msg => console.error(msg),
+  }),
+};
+
+(async () => {
+  await register({
+    packages,
+  }).catch((err) => {
+    const errMessage = 'Encountered an exception.  Exit with process.exit(1).';
+
+    if (registry.packages.logger) {
+      registry.packages.logger.error(err, errMessage);
+    } else {
+      // fallback to console.error if logger packages is not yet registered
+      // eslint-disable-next-line no-console
+      console.error(err, errMessage);
+    }
+
+    process.exit(1);
+  });
+
+  // Can access all the registered packages after register()
+  // Even in another file, just import:
+  // const { registry, package } = require('rejstry');
+  registry.packages.logger.info(registry.packages.config.NODE_ENV);
+})();
+```
--- src/index.js
+++ src/index.js
@@ -1,1 +1,66 @@
-console.log('rejstry');
+/* eslint-disable no-restricted-syntax, no-await-in-loop */
+const fs = require('fs');
+
+const KERNEL_PACKAGES = ['config', 'logger'];
+
+const registry = {
+  packages: {},
+};
+
+const register = async ({
+  packages,
+  packagesFilter,
+}) => {
+  if (typeof packages.config !== 'function') throw new Error('Kernel package config is required.');
+  if (typeof packages.logger !== 'function') throw new Error('Kernel package logger is required.');
+
+  registry.packages.config = packages.config();
+  registry.packages.logger = packages.logger();
+  const { logger } = registry.packages;
+
+  logger.info('Preparing registry...');
+  logger.info('config is registered.');
+  logger.info('logger is registered.');
+
+  const packageIds = Object.keys(packages) || [];
+  const pkgIds = packagesFilter
+    ? packagesFilter(packageIds)
+    : packageIds.filter(packageId => !KERNEL_PACKAGES.includes(packageId));
+  for (const pkgId of pkgIds) {
+    if (pkgId) {
+      logger.info(`Registering packages.${pkgId}.`);
+
+      const pkgRegister = (packages && packages[pkgId]) || undefined;
+      if (pkgRegister) {
+        if (typeof pkgRegister === 'function') {
+          registry.packages[pkgId] = await pkgRegister();
+          registry.packages[pkgId].id = pkgId;
+          logger.info(`packages.${pkgId} is registered.`);
+        } else {
+          logger.warn(`Failed to register.  packages.${pkgId} is not a function.`);
+        }
+      } else {
+        logger.warn(`Failed to register.  packages.${pkgId} not found.`);
+      }
+    }
+  }
+
+  logger.info(Object.keys(registry.packages), 'Packages registered.  Registry is ready.');
+  return registry;
+};
+
+/* eslint-disable global-require, import/no-dynamic-require, no-shadow */
+const loader = dirname => fs.readdirSync(dirname)
+  .filter(dir => dir !== dirname)
+  .reduce((packages, packageDir) => ({
+    ...packages,
+    // packageId is set to package dir
+    [packageDir]: require(`./${packageDir}`),
+  }), {});
+
+module.exports = {
+  register,
+  registry,
+  loader,
+  packages: registry.packages,
+};
